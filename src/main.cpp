b37cc046f4a0b29e850a2b6a48f49955fcac61b3
Amber Goulding <gouldinga1@cardiff.ac.uk>
2025-05-24 23:15:09 +0100

    add clang tidy

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,5 @@ using namespace engine;
 auto main() -> int
 {
   auto answer = util::simple_add(5, 3);
-  std::cout << "The answer is " << answer << std::endl;
+  std::cout << "The answer is " << answer << '\n';
 }
