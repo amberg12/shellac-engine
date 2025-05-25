6c79873c4722dd2d1e3b7d24dc7a07c57ee8b90a
Amber Goulding <gouldinga1@cardiff.ac.uk>
2025-05-25 21:13:02 +0100

    expand to_safe_larger_integral to include enums

--- src/util/to_safe_larger_integral.hpp
+++ src/util/to_safe_larger_integral.hpp
@@ -1,22 +1,38 @@
+#pragma once
+
 #include <cstdint>
 #include <type_traits>
 
 namespace chess {
 
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-struct safe_larger_integral
+template <typename T, typename Enable = void>
+struct safe_larger_integral;
+
+template <typename T>
+struct safe_larger_integral<T, std::enable_if_t<std::is_integral_v<T>>>
 {
-  using type =
-    std::conditional_t<std::is_signed_v<T>, std::int64_t, std::uint64_t>;
+  using type = std::conditional_t<std::is_signed_v<T>, std::int64_t, std::uint64_t>;
 };
 
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+template <typename T>
+struct safe_larger_integral<T, std::enable_if_t<std::is_enum_v<T>>>
+{
+  using underlying = std::underlying_type_t<T>;
+  using type = std::conditional_t<std::is_signed_v<underlying>, std::int64_t, std::uint64_t>;
+};
+
+template <typename T>
 using safe_larger_integral_t = typename safe_larger_integral<T>::type;
 
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-auto to_safe_larger_integral(T e) -> safe_larger_integral_t<T>
+template <typename T>
+constexpr auto to_safe_larger_integral(T e) -> safe_larger_integral_t<T>
 {
-  return static_cast<safe_larger_integral_t<T>>(e);
+  if constexpr (std::is_enum_v<T>) {
+    using TSliInfo = safe_larger_integral<T>;
+    return static_cast<typename TSliInfo::type>(static_cast<typename TSliInfo::underlying>(e));
+  } else {
+    return static_cast<safe_larger_integral_t<T>>(e);
+  }
 }
 
 }  // namespace chess
